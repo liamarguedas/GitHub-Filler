137eab2dcdab87ee85bd7f230e3892d980a0fca5
liamarguedas <iliamftw2013@gmail.com>
2024-07-21 15:11:03 -0300

    add: img files

--- main.py
+++ main.py
@@ -18,7 +18,6 @@ You should have received a copy of the GNU General Public License along with
 GitHub Filler. If not, see <https://www.gnu.org/licenses/>.
 """
 
-
 from pathlib import Path
 import time
 import statistics
