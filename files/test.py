91fbab0d3bed2bf5588fdcf9e0703ca612036581
liamarguedas <iliamftw2013@gmail.com>
2024-07-21 16:50:43 -0300

    add: testing file for init

---
+++ files/test.py
@@ -0,0 +1,19 @@
+"""
+GitHub Filler - Fake Commit Generator for GitHub
+
+Copyright (C) 2024-2024 Liam Arguedas
+
+This file is part of GitHub Filler, a free CLI tool based on the original Commitify 
+designed to generate fake commits for GitHub repositories.
+
+GitHub Filler is distributed under the terms of the GNU General Public License (GPL),
+either version 3 of the License, or any later version.
+
+GitHub Filler is provided "as is", without warranty of any kind, express or implied,
+including but not limited to the warranties of merchantability, fitness for a
+particular purpose, and noninfringement. See the GNU General Public License for
+more details.
+
+You should have received a copy of the GNU General Public License along with
+GitHub Filler. If not, see <https://www.gnu.org/licenses/>.
+"""
